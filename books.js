6c9ade430dbb6c209d356ab218d1aeb4ffc86117
francksefu <90141102+francksefu@users.noreply.github.com>
2023-02-14 23:32:10 +0200

    Add the remove features in books.js

--- books.js
+++ books.js
@@ -1,82 +1,13 @@
 const addBtn = document.getElementById('add');
 const myBooks = document.getElementById('my-books');
-// const myCollection = [];
 
 const title = document.getElementById('title');
 const author = document.getElementById('author');
-// add.addEventListener('click', () => {
-//   const obj = {};
-//   const books = document.createElement('div');
-//   const h4 = document.createElement('h4');
-//   const h5 = document.createElement('h5');
-//   const btn = document.createElement('button');
-//   const hr = document.createElement('hr');
-
-//   myBooks.appendChild(books);
-
-//   h4.textContent = title.value;
-//   h5.textContent = author.value;
-//   btn.textContent = 'Remove';
-
-//   books.appendChild(h4);
-//   books.appendChild(h5);
-//   books.appendChild(btn);
-//   books.appendChild(hr);
-
-//   obj.title = title.value;
-//   obj.author = author.value;
-
-//   myCollection.push(obj);
-
-//   localStorage.setItem('myCollection', JSON.stringify(myCollection));
-
-//   title.value = '';
-//   author.value = '';
-
-//   btn.addEventListener('click', () => {
-//     books.remove();
-//     myCollection.splice(myCollection.indexOf(obj), 1);
-//     localStorage.setItem('myCollection', JSON.stringify(myCollection));
-//   });
-// });
-
-// window.addEventListener('load', () => {
-//   const restore = JSON.parse(localStorage.getItem('myCollection'));
-//   for (let i = 0; i < restore.length; i += 1) {
-//     const books = document.createElement('div');
-//     const h4 = document.createElement('h4');
-//     const h5 = document.createElement('h5');
-//     const btn = document.createElement('button');
-//     const hr = document.createElement('hr');
-//     let tab = myCollection;
-//     myBooks.appendChild(books);
-
-//     h4.textContent = restore[i].title;
-//     h5.textContent = restore[i].author;
-//     btn.textContent = 'Remove';
-
-//     books.appendChild(h4);
-//     books.appendChild(h5);
-//     books.appendChild(btn);
-//     books.appendChild(hr);
-
-//     tab = restore;
-
-//     title.value = '';
-//     author.value = '';
-
-//     btn.addEventListener('click', () => {
-//       books.remove();
-//       tab.splice(i, 1);
-//       localStorage.setItem('myCollection', JSON.stringify(tab));
-//     });
-//   }
-// });
-
 class Books {
   constructor(title, author){
     this.title = title;
     this.author = author;
+    this.obj = {title: this.title, author: this.author};
   } 
 
    static myCollection = [];
@@ -98,13 +29,60 @@ class Books {
         books.appendChild(h5);
         books.appendChild(btn);
         books.appendChild(hr);
+        Books.myCollection.push(this.obj);
+        localStorage.setItem('myCollection', JSON.stringify(Books.myCollection));
+        btn.addEventListener('click', () => {
+          this.removeThing(books);
+          Books.myCollection.splice(Books.myCollection.indexOf(this.obj), 1);
+          localStorage.setItem('myCollection', JSON.stringify(Books.myCollection));
+          //console.log(Books.myCollection.indexOf(this.obj));
+        });
+        
 
   }
 
-  remove() {
-
+  removeThing(bookT) {
+    bookT.remove();
+    
   }
+
+  static loading(myBooksB) {
+    const restore = JSON.parse(localStorage.getItem('myCollection'));
+  for (let i = 0; i < restore.length; i += 1) {
+     const books = document.createElement('div');
+     const h4 = document.createElement('h4');
+     const h5 = document.createElement('h5');
+     const btn = document.createElement('button');
+     const hr = document.createElement('hr');
+     let tab = Books.myCollection;
+     myBooksB.appendChild(books);
+
+    h4.textContent = restore[i].title;
+    h5.textContent = restore[i].author;
+     btn.textContent = 'Remove';
+
+     books.appendChild(h4);
+    books.appendChild(h5);
+    books.appendChild(btn);
+     books.appendChild(hr);
+
+     tab = restore;
+      Books.myCollection = tab;
+     title.value = '';
+     author.value = '';
+
+     btn.addEventListener('click', () => {
+       books.remove();
+       tab.splice(i, 1);
+       localStorage.setItem('myCollection', JSON.stringify(tab));
+     });
+   }
+  }
+
+  
 }
+
+
 
 addBtn.addEventListener('click', () => {
     const title1 = title.value;
@@ -113,6 +91,12 @@ addBtn.addEventListener('click', () => {
     author.value = '';
     const obj = new Books(title1, author1)
     obj.add(myBooks);
-    Books.myCollection.push({title:title1, author:author1});
-    localStorage.setItem('myCollection', JSON.stringify(Books.myCollection));
-})
+    
+
+});
+
+window.addEventListener('load', () => {
+  Books.loading(myBooks);
+});
+
+
